e0b4e906248971b19fd80b0f2efc703f88fdffc8
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-10-21 13:07:41 +0900

    config setting

---
+++ static/js/mqtt-websocket.js
@@ -0,0 +1,36 @@
+var mqtt;
+var reconnectTimeout = 2000;
+var host = "192.168.10.89";
+var port = 9001;
+
+function onConnect(){
+    console.log("연결 성공");
+}
+
+function onFailure(){
+    console.log("연결 실패");
+    //setTimeout(MQTTConnect, reconnectTimeout);
+}
+
+function sendMsg(msg){
+    alert(msg);
+
+    message = new Paho.MQTT.Message(msg);
+    message.destinationName = "iot/led"; 
+    mqtt.send(message);
+}
+
+function MQTTConnect(){
+    console.log("mqtt 연결");
+
+    mqtt = new Paho.MQTT.Client(host,port,"javascript_client"); 
+
+    var options = {
+        timeout:3,
+        userName : "sangjun",
+        password : "tkdwns4455!",
+        onSuccess:onConnect, 
+        onFailure:onFailure,
+    }
+    mqtt.connect(options);
+}
