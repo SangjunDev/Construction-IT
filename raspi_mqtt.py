0d9864a2161e2d129bf7c5a845292a371141fab0
Snagjun Kim <Sangjun.Kim.dev@gmail.com>
2022-11-16 17:24:09 +0900

    mqtt contorl 업데이트

---
+++ raspi_mqtt.py
@@ -0,0 +1,45 @@
+import paho.mqtt.client as mqtt
+import RPi.GPIO as GPIO
+import time
+
+led_pin = 26
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(led_pin,GPIO.OUT)
+
+
+broker = "192.168.10.89"
+port = 9001
+sub_topic = "192.168.10.89/led"
+pub_topic = "192.168.10.89/status"
+
+def on_connect(client, userdata, flags, rc):
+    print("connect.."+str(rc))
+    if rc==0:
+        client.subscribe(sub_topic,0)
+    else:
+        print("연결 실패..")
+
+def on_message(client, userdata, message):
+    myvalue = message.payload.decode("utf-8")
+    print(message.topic+"---"+myvalue)
+    if myvalue == "led_on":
+        GPIO.output(led_pin, GPIO.HIGH)
+        client.publish(pub_topic,"on")
+        print(GPIO.input(led_pin))
+    else:
+        GPIO.output(led_pin, GPIO.LOW)
+        client.publish(pub_topic,"off")
+        print(GPIO.input(led_pin))
+
+if __name__ == "__main__":
+        try:
+         if GPIO.input(led_pin) == 1:
+           print("led state: on")
+         else:
+           print("led state: off")
+
+         mqttClient = mqtt.Client(transport='websockets')
+         mqttClient.on_connect = on_connect
+         mqttClient.on_message = on_message
+         mqttClient.connect(broker ,port ,60)
+         mqttClient.loop_forever()
--- static/js/mqtt-websocket.js
+++ static/js/mqtt-websocket.js
@@ -1,34 +1,46 @@
+$(document).ready(function(){
+    MQTTConnect();
+  });
+  
 var mqtt;
 var reconnectTimeout = 2000;
 var host = "192.168.10.89";
 var port = 9001;
 
+function MQTTConnect(){
+console.log("mqtt 연결");
+
+mqtt = new Paho.MQTT.Client(host,port,"javascript_client"); 
+
+var options = {
+    timeout:3,
+    onSuccess:onConnect, 
+    onFailure:onFailure,
+}
+mqtt.connect(options);
+
+}
+
+
 function onConnect(){
-    console.log("연결 성공");
+  console.log("연결 성공");
+  mqtt.subscribe("192.168.10.89/status")
+  mqtt.onMessageArrived = onMessageArrived;
+
 }
 
 function onFailure(){
-    console.log("연결 실패");
-    //setTimeout(MQTTConnect, reconnectTimeout);
+  console.log("연결 실패");
 }
 
-function sendMsg(msg){
-    //alert(msg);
-
-    message = new Paho.MQTT.Message(msg);
-    message.destinationName = "192.168.10.89/led"; 
-    mqtt.send(message);
+function onMessageArrived(message) {
+console.log("onMessageArrived: " + message.payloadString);
 }
 
-function MQTTConnect(){
-    console.log("mqtt 연결");
+function Msg(msg){
+  //alert(msg);
+  message = new Paho.MQTT.Message(msg);
+  message.destinationName = "192.168.10.89/led";
 
-    mqtt = new Paho.MQTT.Client(host,port,"javascript_client"); 
-
-    var options = {
-        timeout:3,
-        onSuccess:onConnect, 
-        onFailure:onFailure,
-    }
-    mqtt.connect(options);
+  mqtt.send(message);
 }
